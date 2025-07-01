81f5e16b3e093983b8918aedd74354f38738810e
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-07-02 01:12:41 +0530

    add jwt helpers

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -3,9 +3,46 @@ package handlers
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
+
+var secretKey []byte = []byte("top-secret-key-load-from-env")
 
 func generateHash(password string) string {
 	hash := sha256.Sum256([]byte(password))
 	return hex.EncodeToString(hash[:])
+}
+
+func generateJWT(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
 }
