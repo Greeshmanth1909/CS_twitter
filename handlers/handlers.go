5a3f9d194fb0fbb3c27949ee3659113794e33951
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-07-02 19:45:22 +0530

    create new routes

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,10 +8,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 
 	"github.com/Greeshmanth1909/CS_twitter/internal/database"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 
 	_ "github.com/lib/pq"
 )
@@ -29,7 +32,6 @@ func init() {
 		log.Fatal("Error loading .env file")
 	}
 	dbURL := os.Getenv("URL")
-	// open connection to database
 	db, error := sql.Open("postgres", dbURL)
 	if error != nil {
 		fmt.Println(error)
@@ -50,7 +52,7 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
 	ctx := context.TODO()
-	dbRes, err := apiConf.DB.ListUsers(ctx)
+	feed, err := apiConf.DB.GetFeed(ctx)
 
 	if err != nil {
 		fmt.Println(err)
@@ -58,7 +60,41 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("%v", err)))
 	}
 
-	fmt.Println(dbRes)
+	fmt.Println(feed)
+	type ResponseStruct struct {
+		Username string
+		Post     string
+		Comments [][]string
+	}
+	var res []ResponseStruct
+
+	for i := range feed {
+		new := ResponseStruct{}
+		new.Username = feed[i].UsernamePost
+		new.Post = feed[i].Post
+		c := string(feed[i].Comments.([]uint8))
+		d := string(feed[i].CommenterUsernames.([]uint8))
+
+		c = strings.TrimPrefix(c, "{")
+		c = strings.TrimSuffix(c, "}")
+		stringSlice := strings.Split(c, ",")
+		for j := range stringSlice {
+			if len(stringSlice[j]) >= 2 {
+				stringSlice[j] = stringSlice[j][1 : len(stringSlice[j])-1]
+			}
+		}
+
+		d = strings.TrimPrefix(d, "{")
+		d = strings.TrimSuffix(d, "}")
+
+		comments_u := Zip(stringSlice, strings.Split(d, ","))
+		new.Comments = comments_u
+		res = append(res, new)
+	}
+
+	resp, _ := json.Marshal(res)
+
+	w.Write(resp)
 }
 
 // The SignupUser handler checks weather a given username is taken and creates a new entry in the users table if it doesn't exist.
@@ -143,5 +179,69 @@ func LoginUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "application/json")
+	w.Write(resBody)
+}
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	claims := r.Context().Value(jwtClaims).(jwt.MapClaims)
+	username := claims["username"].(string)
+
+	type body struct {
+		Post string `json:"post"`
+	}
+	var req body
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&req)
+
+	var createUserPostParams database.CreateUserPostParams
+	createUserPostParams.Username = username
+	createUserPostParams.Post = req.Post
+
+	ctx := context.TODO()
+	res, err := apiConf.DB.CreateUserPost(ctx, createUserPostParams)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+		return
+	}
+
+	responseJson, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(responseJson)
+}
+
+func CreateComment(w http.ResponseWriter, r *http.Request) {
+	claims := r.Context().Value(jwtClaims).(jwt.MapClaims)
+	username := claims["username"].(string)
+
+	type body struct {
+		Comment string `json:"comment"`
+		PostID  string `json:"post_id"`
+	}
+	var req body
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&req)
+
+	var createUserCommentParams database.CreateUserCommentParams
+	createUserCommentParams.Username = username
+	createUserCommentParams.Comment = req.Comment
+	createUserCommentParams.PostID, _ = uuid.Parse(req.PostID)
+
+	ctx := context.TODO()
+	comment, err := apiConf.DB.CreateUserComment(ctx, createUserCommentParams)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+		return
+	}
+	resBody, _ := json.Marshal(comment)
+
+	w.WriteHeader(http.StatusAccepted)
 	w.Write(resBody)
 }
