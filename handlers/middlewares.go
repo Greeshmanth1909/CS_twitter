590e25d487b6cb26956dad7df03908a992e7b1ba
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-07-03 01:16:08 +0530

    add basic html for frontend

--- handlers/middlewares.go
+++ handlers/middlewares.go
@@ -8,8 +8,9 @@ import (
 
 type ctxKey string
 
-var jwtClaims ctxKey = "claims"
+var jwtClaims ctxKey = "claims" // doing this because context.WithValue does not accept keys with built-in types.
 
+// The AuthMiddleWare middleware is used to authenticate users with a jwt.
 func AuthMiddleWare(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		jwt := r.Header.Get("Authorization")
